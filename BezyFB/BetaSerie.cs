7d2455ddacb7ea5edf4601af70c3163ef89a2ee2
unknown <pepinat@ALB-PC-PIE.report-one.local>
2014-06-17 16:10:46 +0200

    Premier commit

---
+++ BezyFB/BetaSerie.cs
@@ -0,0 +1,18 @@
+﻿// Créer par : pepinat
+// Le : 03-06-2014
+
+using System.Collections.Generic;
+
+namespace BesyFB
+{
+    public class BetaSerie
+    {
+        public List<string> GetListeNouveauxEpisodes()
+        {
+        }
+
+        public string GetPathSousTitre(string episode)
+        {
+        }
+    }
+}
