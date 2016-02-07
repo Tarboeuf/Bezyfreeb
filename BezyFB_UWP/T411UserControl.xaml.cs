7e3fb019177e728c5149db18da045beea50d1e89
Tarboeuf <pierre.epinat@gmail.com>
2016-02-07 15:14:01 +0100

    ajout fonctionnalité t411

---
+++ BezyFB_UWP/T411UserControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using BezyFB_UWP.Lib;
+using BezyFB_UWP.Lib.Helpers;
+using BezyFB_UWP.Lib.T411;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BezyFB_UWP
+{
+    public sealed partial class T411UserControl : UserControl
+    {
+        public T411UserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Download_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            if (await Helper.ShowYesNoDialog("Êtes-vous sûr de vouloir télécharger ce film ?") == YesNo.Yes)
+            {
+                var torrent = DataContext as Torrent;
+                if (null != torrent)
+                {
+                    using (var stream = Settings.Current.T411.DownloadTorrent(torrent.Id))
+                    {
+                        try
+                        {
+                            await Settings.Current.Freebox.DownloadFile(stream, torrent.Name + ".torrent", Settings.Current.PathFilm, false);
+                            Helper.AfficherMessage("Le téléchargement a été rajouté");
+                        }
+                        catch (Exception)
+                        {
+                            Helper.AfficherMessage("Une erreur est survenue lors de l'ajout du téléchargement");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BezyFB_UWP/TorrentDetailPage.xaml.cs
@@ -0,0 +1,179 @@
+﻿using BezyFB_UWP.Lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Store;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using BezyFB_UWP.Lib.T411;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BezyFB_UWP
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class TorrentDetailPage : Page
+    {
+        private static DependencyProperty s_itemProperty
+               = DependencyProperty.Register("Item", typeof(Torrent), typeof(TorrentDetailPage), new PropertyMetadata(null));
+
+        public static DependencyProperty ItemProperty
+        {
+            get { return s_itemProperty; }
+        }
+
+        public Torrent Item
+        {
+            get { return (Torrent)GetValue(s_itemProperty); }
+            set { SetValue(s_itemProperty, value); }
+        }
+
+
+
+        public TorrentDetails Details
+        {
+            get { return (TorrentDetails)GetValue(DetailsProperty); }
+            set { SetValue(DetailsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Details.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DetailsProperty =
+            DependencyProperty.Register("Details", typeof(TorrentDetails), typeof(TorrentDetailPage), new PropertyMetadata(null));
+
+
+
+        public TorrentDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // Parameter is item ID
+            Item = e.Parameter as Torrent;
+
+            Details = await Settings.Current.T411.GetTorrentDetails(Item.Id);
+            View.NavigateToString(Details.Description);
+
+            var backStack = Frame.BackStack;
+            var backStackCount = backStack.Count;
+
+            if (backStackCount > 0)
+            {
+                var masterPageEntry = backStack[backStackCount - 1];
+                backStack.RemoveAt(backStackCount - 1);
+
+                // Doctor the navigation parameter for the master page so it
+                // will show the correct item in the side-by-side view.
+                var modifiedEntry = new PageStackEntry(
+                    masterPageEntry.SourcePageType,
+                    Item,
+                    masterPageEntry.NavigationTransitionInfo
+                    );
+                backStack.Add(modifiedEntry);
+            }
+
+            // Register for hardware and software back request from the system
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void OnBackRequested()
+        {
+            // Page above us will be our master view.
+            // Make sure we are using the "drill out" animation in this transition.
+
+            Frame.GoBack(new DrillInNavigationTransitionInfo());
+        }
+
+        void NavigateBackForWideState(bool useTransition)
+        {
+            // Evict this page from the cache as we may not need it again.
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+
+            if (useTransition)
+            {
+                Frame.GoBack(new EntranceNavigationTransitionInfo());
+            }
+            else
+            {
+                Frame.GoBack(new SuppressNavigationTransitionInfo());
+            }
+        }
+
+        private bool ShouldGoToWideState()
+        {
+            return Window.Current.Bounds.Width >= 720;
+        }
+
+        private void PageRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ShouldGoToWideState())
+            {
+                // We shouldn't see this page since we are in "wide master-detail" mode.
+                // Play a transition as we are navigating from a separate page.
+                NavigateBackForWideState(useTransition: true);
+            }
+            else
+            {
+                // Realize the main page content.
+                FindName("RootPanel");
+            }
+
+            Window.Current.SizeChanged += Window_SizeChanged;
+        }
+
+        private void PageRoot_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Window_SizeChanged;
+        }
+
+        private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            if (ShouldGoToWideState())
+            {
+                // Make sure we are no longer listening to window change events.
+                Window.Current.SizeChanged -= Window_SizeChanged;
+
+                // We shouldn't see this page since we are in "wide master-detail" mode.
+                NavigateBackForWideState(useTransition: false);
+            }
+        }
+
+        private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // Mark event as handled so we don't get bounced out of the app.
+            e.Handled = true;
+
+            OnBackRequested();
+        }
+
+
+    }
+}
