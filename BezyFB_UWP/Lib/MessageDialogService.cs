6acaccdf2011681d1e6e920e1e132b97b072f239
Tarboeuf <pierre.epinat@gmail.com>
2016-05-31 16:29:51 +0200

    fin du refactoring

---
+++ BezyFB_UWP/Lib/MessageDialogService.cs
@@ -0,0 +1,41 @@
+﻿using CommonPortableLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace BezyFB_UWP.Lib
+{
+    public class MessageDialogService : IMessageDialogService
+    {
+        
+        public async Task AfficherMessage(string message)
+        {
+            var md = new MessageDialog(message);
+            await md.ShowAsync();
+        }
+
+        public async Task<DialogResult> ShowYesNoDialog(string content)
+        {
+
+            var dialog = new MessageDialog(content, "Question");
+
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("Yes") { Id = DialogResult.Yes });
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("No") { Id = DialogResult.No });
+
+            //if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily != "Windows.Mobile")
+            //{
+            //    // Adding a 3rd command will crash the app when running on Mobile !!!
+            //    dialog.Commands.Add(new Windows.UI.Popups.UICommand("Maybe later") { Id = 2 });
+            //}
+
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+
+            var result = await dialog.ShowAsync();
+            return (DialogResult)result.Id;
+        }
+    }
+}
---
+++ CommonPortableLib/ApiConnectorHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonPortableLib
+{
+    public static class ApiConnectorHelper
+    {
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
